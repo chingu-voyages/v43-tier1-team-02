f4c90eab977b23864f1371d4f4478318118a1c4f
Petre4488 <44945573+Petre4488@users.noreply.github.com>
2023-03-30 00:53:01 +0300

    How to play page with footer

---
+++ HowToPage/src/Components/App.jsx
@@ -0,0 +1,15 @@
+import MainContent from './MainContent'
+import Footer from './Footer'
+import "./App.css"
+
+
+export default function App() {
+  return (
+    <div className='App'>
+      <MainContent />
+      <Footer />
+    </div>
+  )
+}
+
+
