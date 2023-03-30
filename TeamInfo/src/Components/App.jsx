6462cbad39963f02fc93f8cbd6399df05c373751
Petre4488 <44945573+Petre4488@users.noreply.github.com>
2023-03-30 23:44:01 +0300

    TeamInfo (still working)

---
+++ TeamInfo/src/Components/App.jsx
@@ -0,0 +1,11 @@
+import Footer from "./Footer"
+import MainContent from "./MainContent"
+
+export default function App(){
+    return(
+        <div>
+            <MainContent />
+            <Footer />
+        </div> 
+    )
+}
---
+++ TeamInfo/src/Components/MainContent.jsx
@@ -0,0 +1,11 @@
+import "./MainContent.css"
+
+export default function MainContent(){
+    return(
+        <div>
+            <div className="MadLibsBar">
+                <h1>Mad Libs Game</h1>
+            </div>
+        </div>
+    )
+}
