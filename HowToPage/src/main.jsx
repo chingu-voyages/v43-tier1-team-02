30fff4a57f0ac922fe6420b3b8ff38f62fe6e219
Petre4488 <44945573+Petre4488@users.noreply.github.com>
2023-03-30 23:11:19 +0300

    Footer v1

--- HowToPage/src/main.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './Components/App.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
-)
